7391b02c31eb032b6747cb0deadbf4c298747b02
kovacsbenc3 <kovacsbenc3@gmail.com>
2021-04-29 11:37:27 +0200

    new task error handling

--- todo.ts
+++ todo.ts
@@ -16,9 +16,6 @@ if (cliArguments[2] === undefined) {
   console.log('    -c   Completes an task');
 
 } else if (cliArguments[2] === '-l') {
-
-  
-
   const fs = require('fs');
   let fileContent = fs.readFileSync('list.txt', 'utf-8');
   let array: string[] = fileContent.split('');
@@ -27,9 +24,22 @@ if (cliArguments[2] === undefined) {
   if(array.length === 0){
     console.log('\n No todos for today! :)');
   }console.log('\n'+ fileContent);
-
+  } else{ 
   
-} else if (cliArguments[2] === '-a') {
-  const newTask = cliArguments[3];
-  console.log(`Added new task: ${newTask}`);
-}
+    if (cliArguments[2] === '-a') {
+      if(cliArguments[3] === undefined){
+      console.log('Unable to add: No task provided');
+      } else {
+      const fs = require('fs');
+    fs.writeFileSync('list.txt', '\n',{flag:'a'});
+  
+    let sum: string = '';
+    for(let i: number = 3; i < cliArguments.length; i++){
+      const fs = require('fs');
+      let fileContent = cliArguments[i];
+      fs.writeFileSync('list.txt', fileContent + ' ',{flag:'a'});
+      sum += ' ' + cliArguments[i];
+    }  
+  console.log(`\n New task added: ${sum}`);
+  }
+}}
